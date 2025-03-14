9978d2b851b6d36c09097f6e503bfecacee5bc31
Victor347k <victor347k@gmail.com>
2025-03-14 23:56:36 +0100

    specific rolls can access specific websites

--- src/main/java/com/exambyte/exambyte/config/SecurityConfig.java
+++ src/main/java/com/exambyte/exambyte/config/SecurityConfig.java
@@ -2,19 +2,26 @@ package com.exambyte.exambyte.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity chainBuilder) throws Exception {
         chainBuilder.authorizeHttpRequests(
-                    configurer -> configurer.requestMatchers("/", "/public", "/css/*").permitAll()
+                    configurer -> configurer
+                            .requestMatchers("/css/*").permitAll()
+                            .requestMatchers("/correction").hasRole("Korrektor")
+                            .requestMatchers("/test-management", "/results", "/correction-status").hasRole("Organisator")
                             .anyRequest().authenticated()
-            ).oauth2Login(Customizer.withDefaults());
+            ).oauth2Login(config ->
+                config.userInfoEndpoint(
+                        info -> info.userService(new CustomOAuth2UserService())
+                ));
         return chainBuilder.build();
     }
 }
